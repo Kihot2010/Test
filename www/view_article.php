e82529ee7892e01cfc7112ad62118ae091e2097b
Kihot2010 <kihot.2010@gmail.com>
2015-08-08 23:58:23 +0300

    Добавлены файлы удаления и редактирования новостей, плюс косметика

--- www/view_article.php
+++ www/view_article.php
@@ -1,6 +1,6 @@
 <?php
 
-include './parts/connect.php';
+include './blocks/connect.php';
 $id = $_GET['id'];
 ?>
 
@@ -9,55 +9,58 @@ $id = $_GET['id'];
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Без названия 1</title>
+    <title>Просмотр новости</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css" />
 </head>
 
 <body>
 <div>
-    <table style="width: 100%" cellspacing="0" cellpadding="0">
+    <table style="width: 960px" cellspacing="0" cellpadding="0">
         <tr id="header" >
             <td colspan="2">
-                <?php include './parts/header.php' ?>
+                <?php include './blocks/header.php' ?>
             </td>
         </tr>
         <tr>
             <td id="lf">
-                <?php include './parts/menu.php' ?>
+                <?php include './blocks/menu.php' ?>
             </td>
             <td id="content">
                 <?php
 
 
-                $res = mysql_query("SELECT * FROM articles WHERE id_article='$id '",$db);
+                $res = mysql_query("SELECT * FROM articles WHERE id_article='$id '", $db);
                 $myrow = mysql_fetch_array($res);
 
                 printf("
                 <table>
                     <td>
                         <tr style = 'background-color: bisque; font-size: 15px;'>
-                            <h2>%s</h2>
+                            <br><h2>%s</h2><br>
                         </tr>
 
                         <tr>
                             <p>%s</p>
                         </tr>
+                        <tr>
+                            <br>
+                            <form method='post' action='./blocks/delete.php'>
+                                 <input name='DeleteArticle' type='submit' value='Удалить новость' />
+                            </form>
+                            <form method='post' action='./blocks/edit.php'>
+                                <input name='EditArticle' type='submit' value='Редактировать новость' />
+                            </form>
+                        </tr>
                     </td>
                 </table>",
                 $myrow["title_ar"], $myrow["full_ar"]
-
                 );
-
-
-
-
-
                 ?>
             </td>
         </tr>
         <tr id="botter">
             <td colspan="2">
-                <?php include './parts/botter.php' ?>
+                <?php include './blocks/botter.php' ?>
             </td>
         </tr>
     </table>
